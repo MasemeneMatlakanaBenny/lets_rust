677296d034396ff23ede97e15ebdd802b8cc207f
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-10-21 15:01:03 +0200

    Created a simple greeting file in rust

---
+++ rust_beginner_exercises/exercise_4.rs
@@ -0,0 +1,7 @@
+//Exercise 4: A Rust program that defines a string variable name and assigns it your name and greet you.
+
+fn main(){
+   let name: &str="Benny";
+   println!("Hello {},Welcome to Rust",name);
+
+}
