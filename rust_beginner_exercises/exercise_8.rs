38d11005c0bf0ba60994c4802cda1e5dbf5c0b1c
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-10-23 21:10:09 +0200

    Create exercise_8.rs

---
+++ rust_beginner_exercises/exercise_8.rs
@@ -0,0 +1,9 @@
+//A Rust program that calculates the area of a triangle
+
+fn main()[
+   let height : f32=4.0;
+   let base : f32=5.2;
+   let area_triangle : f32=0.5*height*base;
+   println!("Area of the triangle = {} units squared",area_triangle);
+
+}
