6c89755501e88182c1c166a6cdc734a969c938f4
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-10-23 21:07:22 +0200

    Created exercise6

---
+++ rust_beginner_exercises/exercise_6.rs
@@ -0,0 +1,11 @@
+//A Rust program that declares a global boolean variable named is_sunny -> prints out it is sunny if true else it is not sunny for the day
+
+fn main(){
+   let is_sunny : bool=false;
+   if is_sunny==true{
+     println!("It is sunny for the day");
+  }
+   else {
+     println!("It is not sunny for the day");
+  }
+}
