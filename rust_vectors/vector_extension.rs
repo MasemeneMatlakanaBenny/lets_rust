33b9bd909acf07b5a6fcbcb31d038255473ff6e7
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-10-25 22:01:03 +0200

    Update vector_extension.rs

--- rust_vectors/vector_extension.rs
+++ rust_vectors/vector_extension.rs
@@ -2,7 +2,7 @@
 //Say we want to extend vector A by vector B 
 //Then we can simply do that by the use of extend method:
 
-fn main{
+fn main(){
   let mut  vect_A : Vec<i32> =vec![1,2,3,4,5];
   let vect_B : Vec<i32>  =vec[6,7,8];
   println!("Vector A before extension method: {:?}",vect_A);
