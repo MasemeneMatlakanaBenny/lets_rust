4408f932f8bb486737831163ac39c7854048446c
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-10-25 22:01:15 +0200

    Update vector_length.rs

--- rust_vectors/vector_length.rs
+++ rust_vectors/vector_length.rs
@@ -1,6 +1,6 @@
 //Lets get the vector length in Rust:
 
-fn main{
+fn main(){
   let my_vec : Vec<i32>= vec![21,24,28];
   let vec_size= my_vec.len();
   println!("My vector {:?} has {} elements",my_vec,vec_size);
